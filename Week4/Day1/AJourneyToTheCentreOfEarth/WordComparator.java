9a3f88df0a734223b2498ca3a8290c7e82c91030
Orestis Tanis <47120348+OrestisTanis@users.noreply.github.com>
2020-03-24 09:16:17 +0200

    Update WordComparator.java

--- Week4/Day1/AJourneyToTheCentreOfEarth/WordComparator.java
+++ Week4/Day1/AJourneyToTheCentreOfEarth/WordComparator.java
@@ -12,14 +12,5 @@ public class WordComparator implements Comparator<Entry<String, Integer>>{
             return e1.getKey().compareTo(e2.getKey());
         }
         return result;
-//        int result = o2.getMark() - o1.getMark();
-//        if (result == 0){
-//            result = o1.getAge() - o2.getAge();
-//        }
-//        if (result == 0){
-//            result = o1.getMark() - o2.getMark();
-//            result = o1.getName().compareTo(o2.getName());
-//        }
-//        return result;
     }
 }
